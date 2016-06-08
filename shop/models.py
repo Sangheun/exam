496577a4ac038d9645594abe9ec431324c8373c1
Sangheun Lee <aisiterugh@snu.ac.kr>
2016-06-08 14:49:37 +0900

    Category, Shop, Review 모델 생성 및 마이그레이션

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Shop(models.Model):
+# # 가게명 필드
+#     title = models.CharField(max_length=100)
+#     phonenumber = CharField(max_length=100)
+#     address = models.CharField()
+#     explain = models.TextField()
+#     photo = models.ImageField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+    pass
+
+class Review(models.Model):
+# 관련 Shop
+# 리뷰쓴 유저
+# 남긴 말 필드
+# 인증샷 필드 : 옵션 필드
+# 생성일시
+# 수정일시
+    pass
+
