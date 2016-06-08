a21f88ef38282fb629a9cecb39a082a407b80964
Sangheun Lee <aisiterugh@snu.ac.kr>
2016-06-08 14:34:44 +0900

    shop.views.index 뷰 구현

--- exam/urls.py
+++ exam/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+from django.shortcuts import redirect
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,6 +7,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^shop/', include('shop.urls', namespace='shop')),
+    url(r'^$', lambda request: redirect('/shop/')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
